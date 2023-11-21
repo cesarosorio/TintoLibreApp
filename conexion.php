3a8b0c2992e12cf987438aa1db9c198a72936905
Cesar O <cesarosorio.ie@gmail.com>
2023-11-21 18:31:53 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,5 @@
 <?php
-<<<<<<< HEAD
-// $conexion = new mysqli("localhost","u933403530_tintolibre","wQ0dZN^0f#C41@CG","u933403530_tintolibre");
-$conexion = new mysqli("database","tintoLibreApp","Ac1dl0v3!","tintoLibreApp");
-=======
 $conexion = new mysqli("localhost","u933403530_tintolibre","wQ0dZN^0f#C41@CG","u933403530_tintolibre");
->>>>>>> c7e84626f70169fab3afcb5aab470f8744186c35
 if ($conexion -> connect_errno)
 {
 	die("Fallo conexion:(".$conexion -> mysqli_connect_errno().")".$conexion -> mysqli_connect_errno());
