1a08f37ec75003879803fdb48fb8e7829146dd3b
Cesar Osorio <cesarosorio.ie@gmail.com>
2023-11-21 18:16:30 -0500

    Cambios home

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-// $conexion = new mysqli("localhost","u933403530_tintolibre","wQ0dZN^0f#C41@CG","u933403530_tintolibre");
+// $conexion = new mysqli("localhost","u933403530_tintolibre","wQ0dZN^0f#C41@CG","u933403530_tintolibre");s
 $conexion = new mysqli("database","tintoLibreApp","Ac1dl0v3!","tintoLibreApp");
 if ($conexion -> connect_errno)
 {
