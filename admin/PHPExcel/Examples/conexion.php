9d90f872b05605210580fd71c505b21aaec6c79e
Cesar Osorio <cesarosorio.ie@gmail.com>
2023-11-22 09:56:48 -0500

    Cambios home

--- admin/PHPExcel/Examples/conexion.php
+++ admin/PHPExcel/Examples/conexion.php
@@ -1,5 +1,6 @@
 <?php
-$conexion = new mysqli("localhost","u933403530_tintolibre","wQ0dZN^0f#C41@CG","u933403530_tintolibre");
+#$conexion = new mysqli("localhost","u933403530_tintolibre","wQ0dZN^0f#C41@CG","u933403530_tintolibre");
+$conexion = new mysqli("database","tintoLibreApp","Ac1dl0v3!","tintoLibreApp");
 if ($conexion -> connect_errno)
 {
 	die("Fallo conexion:(".$conexion -> mysqli_connect_errno().")".$conexion -> mysqli_connect_errno());
