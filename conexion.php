3fe09da4a38c3d11392023d9c0183acfc79a7034
Cesar O <cesarosorio.ie@gmail.com>
2023-11-24 12:06:55 -0500

    Delete conexion.php

--- conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conexion = new mysqli("database","tintoLibreApp","Ac1dl0v3!","tintoLibreApp");
-if ($conexion -> connect_errno)
-{
-	die("Fallo conexion:(".$conexion -> mysqli_connect_errno().")".$conexion -> mysqli_connect_errno());
-}
-?>
